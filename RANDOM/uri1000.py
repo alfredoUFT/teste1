7fa8c9a6d81a8260f54e7dd24b3239c41b34face
alfredoUFT <91680091+alfredoUFT@users.noreply.github.com>
2021-10-02 00:37:04 -0300

    Add files via upload

---
+++ RANDOM/uri1000.py
@@ -0,0 +1,12 @@
+#cd onedrive/área de trabalho/random
+print("\n  Bem vindo!\n\n  Favor responder as perguntas abaixo.\n")
+
+name = input("--> Informe seu nome: ")
+age = int(input("--> Informe sua idade: "))
+nick = input("--> Como deseja ser chamado: ")
+
+print(f"\n##\nNome[{name}]  |  Idade[{age}]  |  Apelido[{nick}]\n##\n")
+
+print("  Seja bem vindo,", nick,"!!")
+
+input("  [ Pressione 'enter' para continuar ] ... ")
